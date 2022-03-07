2bc136aeb198d62fd362bb9df1457294e762214b
Took Nothing Left Time <101050324+TNLT@users.noreply.github.com>
2022-03-07 20:03:17 +0100

    3 / ∞
    Repartiment de feina

--- src/AccionsInteligents.java
+++ src/AccionsInteligents.java
@@ -4,11 +4,13 @@ public  class AccionsInteligents {
     //Classe estatica on els sers que tenen acces fan questions sobre el entrorn
 
     public static int PathFinding(Vector2 origen, Vector2 desti, Mon monActual){
+        //TODO: Dejame
         return -1;
     }
 
 
     public static Vector<Animal> Visio(Vector2 origen, Mon monActual){
+        //TODO: TNTL
         return new Vector<Animal>();
     }
 
